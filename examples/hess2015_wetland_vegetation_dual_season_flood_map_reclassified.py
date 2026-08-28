239cffda28fe760b13fd898403ecb2e99412d3d2
Renan Cassimiro Brito <renan.cassimiro91@gmail.com>
2026-08-28 16:48:39 +0000

    Refactoring to a new project strucuture intended to turn the project reproducible with a spec driven soluction

--- examples/hess2015_wetland_vegetation_dual_season_flood_map_reclassified.py
+++ examples/hess2015_wetland_vegetation_dual_season_flood_map_reclassified.py
@@ -16,6 +16,7 @@ import sys
 from datetime import datetime
 from pathlib import Path
 
+
 # Permite importar raspy mesmo sem instalar como pacote
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
